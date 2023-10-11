a64321f0fb4255bcc189ea894665b3a1417ed0f2
JISSHU_BOT <142872501+Jisshubot@users.noreply.github.com>
2023-10-11 21:05:03 +0530

    Update sticker.py

--- plugins/Extra/sticker.py
+++ plugins/Extra/sticker.py
@@ -1,12 +1,10 @@
-#Made
-#by
-#Arsenal Botz
+# Don't Remove Creadits because You are not a repo seller 😁
 
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["stickerid"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
-       await message.reply(f"Sticker ID is  \n {message.reply_to_message.sticker.file_id} \n \n  Unique ID is  \n\n{message.reply_to_message.sticker.file_unique_id}", quote=True)
+       await message.reply(f"Sticker ID is  \n<code>{message.reply_to_message.sticker.file_id}</code> \n \n  Unique ID is  \n\n<code>{message.reply_to_message.sticker.file_unique_id}</code>", quote=True)
     else: 
        await message.reply("<b>Oops !! Not a sticker file</b>")
