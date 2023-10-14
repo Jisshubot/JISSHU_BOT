45fb102df43b5b90ebdcd0c0c0245dd6f8dc0c2b
JISSHU_BOT <142872501+Jisshubot@users.noreply.github.com>
2023-10-14 18:01:03 +0530

    Delete sticker.py

--- sticker.py
+++
@@ -1,4 +0,0 @@
-@Bot.on_message(filters.private & filters.sticker)
-async def stickers(_, message):
-       await message.reply(f"Your Requested Sticker's ID is   * `{message.sticker.file_id}` *", quote=True)
-   
