8d323c3a539219a66261a3e5299b22a92383e405
Junaid khan <146518850+kingkhan11111@users.noreply.github.com>
2023-10-20 17:38:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://zishankhan1:zishankhan1@cluster0.pnwq7om.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://KingKhan:KingKhan@cluster0.mmw7bw2.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
