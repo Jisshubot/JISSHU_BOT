88fd293eeeb9bdfc7dcd82fba95245f970fa5e9b
Junaid khan <146518850+kingkhan11111@users.noreply.github.com>
2023-10-20 17:19:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kingkhan11111/JISSHU-BOT.git /JISSHU-BOT 
+  git clone https://github.com/kingkhan11111/JISSHU-BOT.git/JISSHU-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /JISSHU-BOT 
 fi
-cd /VJ-FILTER-BOT 
+cd /JISSHU-BOT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
