2f2b1e7be2261bec538ec61fee3b2e48a5abedb2
JISSHU_BOT <142872501+Jisshubot@users.noreply.github.com>
2023-10-30 12:23:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -220,7 +220,7 @@ IMDb Data:
 <b>🏷 Title</b>: <a href={url}>{title}</a>
 🎭 Genres: {genres}
 📆 Year: <a href={url}/releaseinfo>{year}</a>
-🌟 Rᴀᴛɪɴɢ: <a href={url}/ratings>{rating}</a>/10"""
+🌟 Rᴀᴛɪɴɢ: <a href={url}/ratings>{rating}</a>
 ⏰Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
 Requested by : {message.from_user.mention}</b>"""
     
