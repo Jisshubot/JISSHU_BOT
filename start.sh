ee36882a223ca9559851ac4a9762af90f0a526be
JISSHU_BOT <142872501+Jisshubot@users.noreply.github.com>
2023-11-01 10:57:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jisshubot/joker-bot.git /joker-bot
+  git clone https://github.com/Jisshubot/JOKER-BOT.git /JOKER-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /joker-bot 
+  git clone $UPSTREAM_REPO /JOKER-BOT
 fi
-cd /joker-bot
+cd /JOKER-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
