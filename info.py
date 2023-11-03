6e30b1c4b189bd7c7d9be0cbb21d76cf36894f3f
Aliya khan <143610535+ELINA-BOTS@users.noreply.github.com>
2023-11-03 14:18:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', 'API_ID'))
-API_HASH = environ.get('API_HASH', 'API_HASH')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID'))
+API_HASH = environ.get('API_HASH')
+BOT_TOKEN = environ.get('BOT_TOKEN')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
