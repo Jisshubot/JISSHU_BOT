7a302376de80e905e1f4a8b2a288cfa972c7ff4d
Aliya khan <143610535+ELINA-BOTS@users.noreply.github.com>
2023-11-09 20:47:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jisshubot/JJISSHU_BOT.git /JISSHU_BOT
+  git clone https://github.com/ELINA-BOTS/JISSHU_BOT.git /JISSHU_BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /JISSHU_BOT
