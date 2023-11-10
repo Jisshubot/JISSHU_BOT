7e8ba5b122757b0b660b927566b0c534c2c414ec
Zishan khan <142872501+Jisshubot@users.noreply.github.com>
2023-11-10 15:28:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('#API_ID'))
+API_ID = int(environ.get('API_ID'))
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
 
