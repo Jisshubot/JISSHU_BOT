02ab5d578caee61e6a4b3daf08e1b2d4ad7f24bf
Aliya khan <143610535+ELINA-BOTS@users.noreply.github.com>
2023-12-18 22:19:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,3 @@
-# Don't Remove Credit @movie_file_20
-# Subscribe YouTube Channel For Amazing Bot @movie_file_20
-# Ask Doubt on telegram @KingVJ01
 
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Jisshubot/JJISSHU_BOT.git /JISSHU_BOT
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /JISSHU_BOT
-fi
-cd /JISSHU_BOT
-pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
